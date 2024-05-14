219f6dc767545cc3c2a53f768c0c61d9f4566083
EiderLabiano <149406571+EiderLabiano@users.noreply.github.com>
2024-05-14 20:57:26 +0200

    Pagina en blanco

--- src/main/java/org/example/demo5/HelloApplication.java
+++ src/main/java/org/example/demo5/HelloApplication.java
@@ -10,14 +10,35 @@ import java.io.IOException;
 import java.sql.*;
 
 public class HelloApplication   extends Application {
+
+    public Stage stage;
+
+    public HelloApplication()
+    {
+        stage = new Stage();
+    }
+
     @Override
+
     public void start(Stage stage) throws IOException {
+        this.stage = stage;
+        pantallaPrincipal();
+    }
+    public void pantallaPrincipal() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
-         stage.setScene(scene);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void pantalla2() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Vacio.class.getResource("vacio.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
         stage.show();
     }
 
 
-    }
+}
