b291f321722050d264c02fab1f77bd92395c63d7
EiderLabiano <149406571+EiderLabiano@users.noreply.github.com>
2024-05-14 20:58:00 +0200

    Update HelloController.java

--- src/main/java/org/example/demo5/HelloController.java
+++ src/main/java/org/example/demo5/HelloController.java
@@ -6,14 +6,20 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Font;
 
+import java.io.IOException;
 import java.sql.*;
 
 public class HelloController {
 
+    public HelloApplication cambioPagina;
     public TextField usuarioText;
     public TextField contrapas;
     public Button butSesion;
 
+    public HelloController()
+    {
+        cambioPagina = new HelloApplication();
+    }
     public Connection conectarSql() throws SQLException {
         String direccion = "jdbc:mysql://localhost:3306/programacion";
         String usuario = "root";
@@ -24,14 +30,18 @@ public class HelloController {
             conexion = DriverManager.getConnection(direccion, usuario, contrasenya);
             String contra= contrapas.getText();
             String nomb = usuarioText.getText();
+            cambioPagina.pantalla2();
             if (conexion != null) {
                 return conexion;
             }
         } catch (SQLException e) {
             System.out.println("Error al acceder");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
         return null;
     }
+
 
 
 }
