8b1fb23ad1cc219375185dd4a30d44f23b17c3f2
EiderLabiano <149406571+EiderLabiano@users.noreply.github.com>
2024-05-14 20:36:52 +0200

    Conexion con base de datos

--- src/main/java/org/example/demo5/HelloController.java
+++ src/main/java/org/example/demo5/HelloController.java
@@ -1,14 +1,37 @@
 package org.example.demo5;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+
+import java.sql.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public TextField usuarioText;
+    public TextField contrapas;
+    public Button butSesion;
+
+    public Connection conectarSql() throws SQLException {
+        String direccion = "jdbc:mysql://localhost:3306/programacion";
+        String usuario = "root";
+        String contrasenya = "root";
+        Connection conexion = null;
+
+        try {
+            conexion = DriverManager.getConnection(direccion, usuario, contrasenya);
+            String contra= contrapas.getText();
+            String nomb = usuarioText.getText();
+            if (conexion != null) {
+                return conexion;
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al acceder");
+        }
+        return null;
     }
+
+
 }
