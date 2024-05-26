a1512b54abe576fa07263c3e0b8baed80a23289e
EiderLabiano <149406571+EiderLabiano@users.noreply.github.com>
2024-05-26 12:05:48 +0200

    Update HelloController.java

--- src/main/java/org/example/demo5/HelloController.java
+++ src/main/java/org/example/demo5/HelloController.java
@@ -20,7 +20,7 @@ public class HelloController {
 
     public HelloApplication cambioPagina;
     public TextField usuarioText;
-    public TextField contrapas;
+    public PasswordField contrapas;
     public Button butSesion;
     public Button refreshButton;
     public ListView<String> componentes = new ListView<>();
@@ -31,24 +31,30 @@ public class HelloController {
     {
         cambioPagina = new HelloApplication();
     }
-    public void cambio() throws IOException {
+    public void cambio(ActionEvent event) throws IOException {
+        cerrarVentana(event);
         cambioPagina.TABLA();
     }
-    public Connection conectarSql() {
-        String direccion = "jdbc:mysql://localhost:3306/TallerMecanico";
-        String usuario = "root";
-        String contrasenya = "root";
-        Connection conexion = null;
+    public boolean conectarSql(ActionEvent event) throws SQLException, IOException {
+        Conexion connection = new Conexion();
 
-        try {
-            conexion = DriverManager.getConnection(direccion, usuario, contrasenya);
-            if (conexion != null) {
-                return conexion;
+        connection.statement.execute("use TallerMecanico");
+        String nombre = usuarioText.getText();
+        String contrasenya = contrapas.getText();
+
+        ResultSet resultSet = connection.statement.executeQuery("select Nombre, Contraseña from usuarios where Nombre = '" + nombre + "' AND Contraseña = '" + contrasenya + "'");
+        if (resultSet.next()) {
+            String nombreEnBaseDeDatos = resultSet.getString("Nombre");
+            if (nombreEnBaseDeDatos.equals(nombre))
+            {
+                cambio(event);
+                return true;
             }
-        } catch (SQLException e) {
-            System.out.println("Error al acceder");
+        } else {
+            error1();
+            return false;
         }
-        return null;
+        return false;
     }
 
     public static void cerrarVentana(ActionEvent e) {
@@ -59,97 +65,8 @@ public class HelloController {
     public void error1() {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setHeaderText("Error");
-        alert.setContentText("Formato de usuario incorrecto.");
-        alert.setTitle("IMPOSIBLE AÑADIR TRABAJADOR");
+        alert.setContentText("El usuario con el que está intentando logearse no tiene permisos para acceder a la aplicación, contacte con el CAU para poder solucionarlo.");
+        alert.setTitle("IMPOSIBLE INICIAR SESION");
         alert.show();
     }
-
-
-    public void error() {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setHeaderText("Error");
-        alert.setContentText("Formato de  contraseña incorrecta.");
-        alert.setTitle("IMPOSIBLE AÑADIR TRABAJADOR");
-        alert.show();
-    }
-
-    public void Informacion() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setHeaderText("Mensaje");
-        alert.setContentText("Empleado " + usuarioText.getText() + " introducido en la base de datos");
-        alert.setTitle("HECHO");
-        alert.show();
-    }
-
-    public boolean userExists(String username, String password) throws SQLException {
-        Connection conexion = conectarSql();
-        PreparedStatement statment = conexion.prepareStatement("SELECT COUNT(*) FROM usuario WHERE nombre = ? AND contrasenya = ?");
-        statment.setString(1, username);
-        statment.setString(2, password);
-        ResultSet resultSet = statment.executeQuery();
-        resultSet.next();
-        int count = resultSet.getInt(1);
-        return count > 0;
-    }
-
-    public void refresh() {
-        try {
-            Connection connection = conectarSql();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from Componentes");
-            componentes.getItems().clear();
-            while (resultSet.next()) {
-                componentes.getItems().add(resultSet.getString("nombre"));
-            }
-        } catch (SQLException e) {
-            System.out.println("Error al refrescar");
-        }
-    }
-
-    public void initialize() {
-        componentes.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                String compo = componentes.getSelectionModel().getSelectedItem();
-                refrescar();
-            }
-        });
-    }
-    public void refrescar()
-    {
-        try
-        {
-            Connection connection = conectarSql();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT NOMBRE FROM Componentes");
-            componentes.getItems().clear();
-            while (resultSet.next())
-            {
-                componentes.getItems().add(resultSet.getString("NOMBRE"));
-            }
-        } catch (SQLException e) {
-            System.out.println("Error al refrescar");
-        }
-    }
-    /*public void datos(String nombre)
-    {
-        {
-            try {
-                Connection connection = conectarSql();
-                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Componentes where NOMBRE = ?");
-                preparedStatement.setString(1, nombre);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                while (resultSet.next())
-                {
-                    IDLabel.setText(resultSet.getString("ID"));
-                    NombreLabel.setText(resultSet.getString("NOMBRE"));
-                    PuestoLabel.setText(resultSet.getString("PUESTO"));
-                    SalarioLabel.setText(resultSet.getString("SALARIO"));
-                    FechaLabel.setText(resultSet.getString("FECHA"));
-                }
-            } catch (SQLException e) {
-                System.out.println("Error al Seleccionar");
-            }
-        }
-    }*/
 }
